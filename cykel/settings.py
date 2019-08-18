b32b677b513ad4b99f347a97c74805ae61767aa0
Maximilian Richt <maxi@richt.name>
2019-08-18 23:27:26 +0200

    ALLOWED_HOSTS configurable per env

--- cykel/settings.py
+++ cykel/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
