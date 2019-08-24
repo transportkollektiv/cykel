fd394db40eb3ca59b6f2b29220cbae49aa18626f
Maximilian Richt <maxi@richt.name>
2019-08-24 03:12:45 +0200

    fix missing import

--- cykel/urls.py
+++ cykel/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 
 urlpatterns = [
