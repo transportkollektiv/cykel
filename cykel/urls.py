2e1dc682d2786c556a5d196e81ec8abc4a32fa7f
Maximilian Richt <maxi@richt.name>
2019-08-30 15:32:53 +0200

    redirect / to UI_URL

--- cykel/urls.py
+++ cykel/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from django.conf.urls import url
 from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(permanent=False, url=settings.UI_URL)),
     path('bikesharing/', include('bikesharing.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
