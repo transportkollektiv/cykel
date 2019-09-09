18627da7b4f2652f856dda347158c88ad7f86100
ubahnverleih <ubahnverleih@gmail.com>
2019-09-09 17:12:40 +0200

    configuration for hiding older bikes

--- bikesharing/models.py
+++ bikesharing/models.py
@@ -122,3 +122,11 @@ class Station(models.Model):
 
 class BikeSharePreferences(Preferences):
     station_match_max_distance = models.IntegerField(default=20)
+    gbfs_hide_bikes_after_location_report_silence = models.BooleanField(
+    	default=False,
+    	help_text="If activated, vehicles will disappear from GBFS, if there was no location report in the configured time period."
+    )
+    gbfs_hide_bikes_after_location_report_hours = models.IntegerField(
+    	default=1,
+    	help_text="Time period (in hours) after the vehicles will hidden from GBFS, if there was no location report. Needs 'Gbfs hide bikes after location report silence' activated."
+    )
