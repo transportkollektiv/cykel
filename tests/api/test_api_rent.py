03107be018271e73778faeae5760e3b3ab097f8a
ubahnverleih <ubahnverleih@gmail.com>
2020-05-15 13:59:22 +0200

    tests for renting bikes without rights

---
+++ tests/api/test_api_rent.py
@@ -0,0 +1,50 @@
+import pytest
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import Permission
+from bikesharing.models import Bike
+
+@pytest.fixture
+def testuser_john_doe(django_user_model):
+    return django_user_model.objects.create(username="john", password="doe")
+
+@pytest.fixture
+def user_client_john_doe_logged_in(testuser_john_doe):
+    client = APIClient()
+    token, _ = Token.objects.get_or_create(user=testuser_john_doe)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    return client
+
+@pytest.fixture
+def available_bike():
+    return Bike.objects.create(availability_status='AV', bike_number='1337')
+
+@pytest.fixture
+def disabled_bike():
+    return Bike.objects.create(availability_status='DI', bike_number='2342')
+
+@pytest.fixture
+def inuse_bike():
+    return Bike.objects.create(availability_status='IU', bike_number='8080')
+
+
+@pytest.mark.django_db
+def test_start_rent_logged_in_without_renting_rights(testuser_john_doe, user_client_john_doe_logged_in, available_bike):
+    data = {
+        'bike_number': '1337'
+    }
+    response = user_client_john_doe_logged_in.post('/api/rent/start', data)
+    assert response.status_code == 403, response.content
+    available_bike.refresh_from_db()
+    assert available_bike.availability_status == 'AV'
+
+@pytest.mark.django_db
+def test_start_rent_logged_out(available_bike):
+    data = {
+        'bike_number': '1337'
+    }
+    client = APIClient()
+    response = client.post('/api/rent/start', data)
+    assert response.status_code == 401, response.content
+    available_bike.refresh_from_db()
+    assert available_bike.availability_status == 'AV'
