76df65b6dcba12dff9d3694991ee0b5e07a661d8
Maximilian Richt <maxi@richt.name>
2020-10-28 14:45:57 +0100

    chore: upgrade to django 3.1.2

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 from rest_framework.authtoken import views
 
@@ -16,7 +15,7 @@ router.register(r"rent", RentViewSet, basename="rent")
 router.register(r"maintenance", MaintenanceViewSet, basename="maintenance")
 
 urlpatterns = [
-    url(r"^", include(router.urls)),
+    re_path(r"^", include(router.urls)),
     path("bike/updatelocation", updatebikelocation),
     path("user", UserDetailsView.as_view()),
     path("config/loginproviders", LoginProviderViewSet.as_view({"get": "list"})),
