ce15b37af366e555be918c32cc598cef1bd14288
Maximilian Richt <maxi@richt.name>
2020-12-06 18:26:46 +0100

    refactor: split cykel models into own files

---
+++ cykel/models/cykel_log_entry.py
@@ -0,0 +1,26 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class CykelLogEntry(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(db_index=True)
+    content_object = GenericForeignKey("content_type", "object_id")
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
+    action_type = models.CharField(max_length=200)
+    data = models.JSONField(null=True, blank=True)
+
+    class Meta:
+        ordering = ("-timestamp",)
+        verbose_name = "Log Entry"
+        verbose_name_plural = "Log Entries"
+
+    def delete(self, using=None, keep_parents=False):
+        raise TypeError("Logs cannot be deleted.")
+
+    def __str__(self):
+        return (
+            f"CykelLogEntry(content_object={self.content_object}, "
+            + f"action_type={self.action_type}, timestamp={self.timestamp})"
+        )
---
+++ cykel/models/user_manager.py
@@ -0,0 +1,59 @@
+from django.contrib import auth
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        """Create and save a user with the given username, and password."""
+        if not username:
+            raise ValueError("The given username must be set")
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, password, **extra_fields)
+
+    def with_perm(
+        self, perm, is_active=True, include_superusers=True, backend=None, obj=None
+    ):
+        if backend is None:
+            backends = auth._get_backends(return_tuples=True)
+            if len(backends) == 1:
+                backend, _ = backends[0]
+            else:
+                raise ValueError(
+                    "You have multiple authentication backends configured and "
+                    "therefore must provide the `backend` argument."
+                )
+        elif not isinstance(backend, str):
+            raise TypeError(
+                "backend must be a dotted import path string (got %r)." % backend
+            )
+        else:
+            backend = auth.load_backend(backend)
+        if hasattr(backend, "with_perm"):
+            return backend.with_perm(
+                perm,
+                is_active=is_active,
+                include_superusers=include_superusers,
+                obj=obj,
+            )
+        return self.none()
